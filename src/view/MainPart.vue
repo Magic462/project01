2c07646abc77879b71fba12d65a1ab3113e0104a
skkka <2274270031@qq.com>
2024-10-31 22:00:48 +0800

    MainPart02

--- src/view/MainPart.vue
+++ src/view/MainPart.vue
@@ -1,18 +1,51 @@
 <script setup>
-
+const leftData=[
+  '人工智能',
+  '关注',
+  'Web前端',
+  'Server后台',
+  'iOS',
+  'Android'
+]
+const midData = [
+  
+]
 </script>
 
 
   <template>
-  <el-row :gutter="10">
-    <el-col :offset="1" :xs="8" :sm="6" :md="4" :lg="30" :xl="1">
-      <div class="grid-content ep-bg-purple" />
+  <el-row :gutter="6">
+    <el-col :lg="3" class="left-col">
+      <ul class="grid-content ep-bg-purple" style="padding: 0;">
+        <li  v-for="(item,index) in leftData" :key="index" class="l-child">{{ item}}</li>
+      </ul>
     </el-col>
-    <el-col :offset="5" :xs="10" :sm="6" :md="8" :lg="10" :xl="11">
-      <div class="grid-content ep-bg-purple-light" />
+    <el-col :lg="13" class="mid-col">
+      <ul class="grid-content ep-bg-purple-light" style="padding: 0;">
+        <li class="first">
+          <div class="one" style="
+           display: inline-block;
+           font-size: 16px;
+           margin-left: 15px;
+          margin-right: 33px;
+          ">推荐</div>
+           
+          <div class="two" style=" display: inline-block;
+  font-size: 16px;">最新</div>
+        </li>
+        <li class="m-child"></li>
+        <li class="m-child"></li>
+        <li class="m-child"></li>
+        <li class="m-child"></li>
+        <li class="m-child"></li>
+        <li class="m-child"></li>
+        <li class="m-child"></li>
+      </ul>
     </el-col>
-    <el-col :xs="4" :sm="6" :md="8" :lg="4" :xl="11">
-      <div class="grid-content ep-bg-purple" />
+    <el-col  :lg="4" class="rig-col">
+      <div class="grid-content ep-bg-purple">
+        
+      </div>
     </el-col>
   </el-row>
 
@@ -29,7 +62,53 @@
 
 .grid-content {
   border-radius: 4px;
-  min-height: 90vh;
+   min-height: 90vh;/*//////////////////////最后删除掉///////////////// */
   background-color: aqua;
+}
+.el-col.left-col{
+  margin-left: 7vw;
+  margin-right: 1.5vw;
+  /* min-height: 50vh; */
+}
+.left-col.grid-content,
+.mid-col.grid-content,
+.rig-col.grid-content{
+  display: flex;
+  flex-direction: column;
+}
+
+.l-child{
+  background-color: red;
+  height: 9vh;
+  flex: 1;
+  padding: 0.5vh;
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+}
+.rig-col{
+  margin-left: 1.5vw;
+}
+.first{
+  display: flex;
+  flex-direction: row;
+  height: 48px;
+  justify-content: left; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+}
+/* .first.one
+.first.two{
+  display: inline-block;
+  font-size: 20px;
+}
+.first.one{
+  margin-left: 10px;
+  margin-right: 10px;
+} */
+.m-child{
+  background-color: green;
+  display: flex;
+  height: 18vh;
+  flex: 1;
 }
 </style>
